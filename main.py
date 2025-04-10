bc159f756fefc4091add887ab22ce079df36721c
ananya-a11y <ananyaahlawat56@gmail.com>
2025-04-10 16:20:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -61,6 +61,7 @@ def home():
     return render_template("index.html", suggestions=suggestions)
 
 # Run Flask app on correct port for Render
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)
+
+    
