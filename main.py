a88101131660121d2bec91c36a5b0ee42f818b27
ananya-a11y <ananyaahlawat56@gmail.com>
2025-04-10 16:27:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -56,10 +56,12 @@ def get_suggestions():
 
 @app.route("/")
 @app.route("/home")
+@app.route('/')
 def home():
-    suggestions = get_suggestions()
-    return render_template("index.html", suggestions=suggestions)
+    return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
 
